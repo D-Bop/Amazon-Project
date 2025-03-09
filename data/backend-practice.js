3957be7019b2734beebe35e1c49e2b8ed36b4749
D-Bop <official.og.owolabi@gmail.com>
2025-03-09 12:03:44 +0100

    we created a load proudcts function and gave it a callback (loading products from the backend

--- data/backend-practice.js
+++ data/backend-practice.js
@@ -1,9 +1,13 @@
+// creating a new http request
 const xhr = new XMLHttpRequest();
 
+// loading the message from the backend
 xhr.addEventListener('load', () => {
     console.log(xhr.response)
 })
 
+//  typing the type of request to send to the backend using the 'Get" request and also the backend url
 xhr.open('GET', "https://supersimplebackend.dev");
+
+// sending the message to the backend
 xhr.send();
-// xhr.response
