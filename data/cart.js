aad977ab97ba4b9e41cb004172a53c4260f1a9ac
D-Bop <official.og.owolabi@gmail.com>
2025-03-17 09:37:37 +0100

    learned promises and converted callbacks to promises

--- data/cart.js
+++ data/cart.js
@@ -68,3 +68,22 @@ export function updateDeliveryOption(productId, deliveryOptionId) {
 
     saveToStrorage();
 }
+
+
+
+
+export function loadCart(fun) {
+  const xhr = new XMLHttpRequest()
+
+
+  xhr.addEventListener("load", () => {
+    console.log(xhr.response)
+
+    fun();
+  });
+ 
+  
+
+  xhr.open('GET', 'https://supersimplebackend.dev/cart');
+  xhr.send();
+}
