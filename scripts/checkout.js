7c968a20ce401a6f2aefe5a9b273198e9879d273
D-Bop <official.og.owolabi@gmail.com>
2025-03-17 10:43:08 +0100

    converted promises to async await

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -5,18 +5,33 @@ import { loadCart } from "../data/cart.js";
 // import '../data/cart-class.js';
 // import '../data/backend-practice.js'
 
-Promise.all([
-    loadProductsFetch(),
-    new Promise((resolve) => {
+async function loadPage() {
+
+    await loadProductsFetch();
+
+    await new Promise((resolve) => {
         loadCart(() => {
             resolve();
         });
-    })
-
-]).then((values) => {
+    });
+    
     renderOrderSummary();
     renderPaymentSummary();
-});
+}
+loadPage();
+
+// Promise.all([
+//     loadProductsFetch(),
+//     new Promise((resolve) => {
+//         loadCart(() => {
+//             resolve();
+//         });
+//     })
+
+// ]).then((values) => {
+//     renderOrderSummary();
+//     renderPaymentSummary();
+// });
 
 // new Promise((resolve) => {
 //     loadProducts(() => {
